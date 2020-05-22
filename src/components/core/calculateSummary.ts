ac002db99fa2420e0368e7520a5db4e160bf86a7
Dima Solovey <vlnsk@ukr.net>
2020-05-22 16:59:16 +0300

    Use lodash

--- src/components/core/calculateSummary.ts
+++ src/components/core/calculateSummary.ts
@@ -1,9 +1,10 @@
+import fill from "lodash/fill";
 import { Grid } from "../redux/reducers";
 
 export default (grid: Grid): [number[], number[]] => {
     const gridLength = grid.length;
-    const rowSumms: number[] = Array(gridLength).fill(0, 0, gridLength);
-    const colSumms: number[] = Array(gridLength).fill(0, 0, gridLength);
+    const rowSumms: number[] = fill(Array(gridLength), 0);
+    const colSumms: number[] = fill(Array(gridLength), 0);
 
     grid.forEach((row:number[], yCoordinate) => {
         row.forEach((col:number, xCoordinate) => {
--- src/components/core/gridGenerator.ts
+++ src/components/core/gridGenerator.ts
@@ -1,3 +1,5 @@
+import fill from "lodash/fill";
+
 function getRandomInt(max: number, min: number = 1): number {
     return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -6,19 +8,18 @@ function throwCoin(): boolean {
     return Math.random() > 0.5;
 }
 
-export default function (dimension: number): [number[], number[]] {
-    const result = Array(dimension);
-    const fulfilled = Array(dimension);
+export default function (dimension: number): [number[][], number[][]] {
+    const result = fill(Array(dimension), [] as number[]);
+    const fulfilled = fill(Array(dimension), [] as number[]);
 
     for (let y = 0; y < dimension; y++) {
-        result[y] = Array(dimension);
-        fulfilled[y] = Array(dimension);
+        result[y] = fill(Array(dimension), 0);
+        fulfilled[y] = fill(Array(dimension), 0);
 
         for (let x = 0; x < dimension; x++) {
             if (throwCoin()) {
                 result[y][x] = fulfilled[y][x] = getRandomInt(20);
             } else {
-                result[y][x] = 0;
                 fulfilled[y][x] = getRandomInt(20);
             }
         }
